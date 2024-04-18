23039ed7313f8126e45c363a160ce55827b66182
simo.zgaoua <m.zgaoua2001@gmail.com>
2024-04-18 22:18:56 +0100

    CircuitResponse CircuitController CityController

---
+++ src/main/java/com/pfa/BackendPFA/model/CircuitResponse.java
@@ -0,0 +1,19 @@
+package com.pfa.BackendPFA.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CircuitResponse {
+
+    private int circuitId;
+    private String cityName;
+    private Date departureTime;
+    private int departureMonumentId;
+}
